5d5d6530ee9a626337665c4ad4d1b8ca095e86b7
Shefali Sanwal <shefalisanwal1@gmail.com>
2025-10-28 10:39:14 -0500

    Bare bones in Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,1 +1,8 @@
-
+export default function Register(props) {
+    return (
+        <div>
+            <h1>Welcome to Pomocat's Register page!</h1>
+            <p>This is a work in progress, so come back soon!</p>
+        </div>
+    );
+}
