1aaa01fbb99569c809308e0d895e40c9fc41d0d9
nitya <nityamadadi20@gmail.com>
2025-10-27 20:32:33 -0500

    added routes

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,5 @@
+export default function Calendar(props) {
+    return <div>
+        <h1>This is the Calendar!</h1>
+    </div>
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home(props) {
+    return <div>
+        <h1>This is the Home Page!</h1>
+    </div>
+}
