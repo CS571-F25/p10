9e69ed1b0501823057738de0b0b544d3f2aeb6c2
Shefali Sanwal <shefalisanwal1@gmail.com>
2025-10-28 10:38:44 -0500

    Bare bones of SignIn.jsx

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,1 +1,8 @@
-
+export default function SignIn(props) {
+    return (
+        <div>
+            <h1>Welcome to Pomocat's Sign In page!</h1>
+            <p>This is a work in progress, so come back soon!</p>
+        </div>
+    );
+}
